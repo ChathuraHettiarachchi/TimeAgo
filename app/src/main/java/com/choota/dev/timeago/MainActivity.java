8d414df690546c980b6cc2b822f6c2f1828dfcc9
Chathura Hettiarachchi <Chathura.h@fidenz.com>
2016-05-15 03:26:10 +0530

    Added to jcenter 1.1.5

---
+++ app/src/main/java/com/choota/dev/timeago/MainActivity.java
@@ -0,0 +1,35 @@
+package com.choota.dev.timeago;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.choota.dev.ctimeago.TimeAgo;
+
+import org.joda.time.DateTime;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv_time, tv_timeago, tv_time1, tv_timeago1;
+
+        tv_time = (TextView) findViewById(R.id.tv_time);
+        tv_timeago = (TextView) findViewById(R.id.tv_timeago);
+
+        tv_time1 = (TextView) findViewById(R.id.tv_time1);
+        tv_timeago1 = (TextView) findViewById(R.id.tv_timeago1);
+
+        tv_time.setText(new Date().toString());
+        tv_time1.setText(new DateTime(new Date()).minusDays(1).toDate().toString());
+
+        tv_timeago.setText(new TimeAgo().getTimeAgo(new Date()));
+        tv_timeago1.setText(new TimeAgo().getTimeAgo(new DateTime(new Date()).minusDays(2).toDate()));
+
+    }
+}
---
+++ timezago/src/main/java/com/choota/dev/ctimeago/TimeAgo.java
@@ -0,0 +1,77 @@
+package com.choota.dev.ctimeago;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by ChathuraHettiarachchi on 1/29/16.
+ */
+public class TimeAgo {
+
+    SimpleDateFormat simpleDateFormat, dateFormat;
+    DateFormat timeFormat;
+    Date dateTimeNow;
+    String timeFromData;
+    String pastDate;
+    String sDateTimeNow;
+
+    private static final int SECOND_MILLIS = 1000;
+    private static final int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+    private static final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+    private static final int DAY_MILLIS = 24 * HOUR_MILLIS;
+    private static final int WEEKS_MILLIS = 7 * DAY_MILLIS;
+    private static final int MONTHS_MILLIS = 4 * WEEKS_MILLIS;
+    //private static final int YEARS_MILLIS = 12 * MONTHS_MILLIS;
+
+    public TimeAgo() {
+
+        simpleDateFormat = new SimpleDateFormat("dd/M/yyyy HH:mm:ss");
+        dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        timeFormat = new SimpleDateFormat("h:mm aa");
+
+        Date now = new Date();
+        sDateTimeNow = simpleDateFormat.format(now);
+
+        try {
+            dateTimeNow = simpleDateFormat.parse(sDateTimeNow);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getTimeAgo(Date startDate) {
+
+        //  date counting is done till todays date
+        Date endDate = dateTimeNow;
+
+        //  time difference in milli seconds
+        long different = endDate.getTime() - startDate.getTime();
+
+        // TODO: localize
+        if (different < MINUTE_MILLIS) {
+            return "just now";
+        } else if (different < 2 * MINUTE_MILLIS) {
+            return "a min ago";
+        } else if (different < 50 * MINUTE_MILLIS) {
+            return different / MINUTE_MILLIS + " mins ago";
+        } else if (different < 90 * MINUTE_MILLIS) {
+            return "an hour ago";
+        } else if (different < 24 * HOUR_MILLIS) {
+            timeFromData = timeFormat.format(startDate);
+            return timeFromData;
+        } else if (different < 48 * HOUR_MILLIS) {
+            return "yesterday";
+        } else if (different < 7 * DAY_MILLIS){
+            return different / DAY_MILLIS + " days ago";
+        } else if (different < 2 * WEEKS_MILLIS){
+            return different / WEEKS_MILLIS + " week ago";
+        } else if (different < 3.5 * WEEKS_MILLIS){
+            return different / WEEKS_MILLIS + " weeks ago";
+        } else {
+            pastDate = dateFormat.format(startDate);
+            return pastDate;
+        }
+    }
+}
